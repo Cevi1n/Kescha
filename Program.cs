6b77b25975de6c71408e94a0ac712be5e944fbf1
Cevi1n <samandaribragimov0775@gmail.com>
2023-10-10 21:19:40 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+System.Console.Write("Enter your name: ");
+string name = System.Console.ReadLine();
+string greeting = $"Hello, {name}";
+System.Console.WriteLine(greeting);
+
+System.Console.Write("Enter your age: ");
+string ageAstring = Console.ReadLine();
+System.Console.WriteLine("Converting...");
+int age = Convert.ToInt32(ageAstring);
+System.Console.WriteLine($"Successfully converted! {age}");
+int Keshage = 3;
+int AgeDifference = age - Keshage;
+System.Console.WriteLine($"The difference between your and Kescha's age is {AgeDifference}");
+
